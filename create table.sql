d2140772bdf8c043959dbabac93f250e619d475c
m-bikko <90394622+m-bikko@users.noreply.github.com>
2023-04-24 06:52:05 +0600

    Update create table.sql
    Changed all the e-commerce structures. Improved the data store.

--- create table.sql
+++ create table.sql
@@ -1,67 +1,86 @@
-CREATE DATABASE ecommerce;
-
-USE ecommerce;
-
-CREATE TABLE customers (
-  customer_id INT PRIMARY KEY,
-  name VARCHAR(50),
-  email VARCHAR(100),
-  phone VARCHAR(20),
-  address VARCHAR(200)
+CREATE TABLE users1 (
+    user_id NUMBER PRIMARY KEY,
+    username VARCHAR2(50) NOT NULL,
+    email VARCHAR2(100) UNIQUE NOT NULL,
+    password1 VARCHAR2(100) NOT NULL
 );
 
-CREATE TABLE orders (
-  order_id INT PRIMARY KEY,
-  customer_id INT,
-  order_date DATE,
-  total_amount DECIMAL(10,2),
-  payment_method VARCHAR(50),
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
-
-CREATE TABLE order_items (
-  order_item_id INT PRIMARY KEY,
-  order_id INT,
-  product_id INT,
-  quantity INT,
-  price DECIMAL(10,2),
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (product_id) REFERENCES products(product_id)
-);
-
-CREATE TABLE products (
-  product_id INT PRIMARY KEY,
-  product_name VARCHAR(50),
-  description TEXT,
-  image_url VARCHAR(200),
-  price DECIMAL(10,2),
-  stock_quantity INT,
-  category_id INT,
-  supplier_id INT,
-  FOREIGN KEY (category_id) REFERENCES categories(category_id),
-  FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
+CREATE TABLE addresses (
+    address_id NUMBER PRIMARY KEY,
+    user_id NUMBER,
+    street VARCHAR2(100) NOT NULL,
+    city VARCHAR2(50) NOT NULL,
+    state1 VARCHAR2(50) NOT NULL,
+    zip_code VARCHAR2(10) NOT NULL,
+    country VARCHAR2(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users1(user_id)
 );
 
 CREATE TABLE categories (
-  category_id INT PRIMARY KEY,
-  category_name VARCHAR(50)
+    category_id NUMBER PRIMARY KEY,
+    name1 VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE manufacturers (
+    manufacturer_id NUMBER PRIMARY KEY,
+    name1 VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE payment_methods (
+    payment_method_id NUMBER PRIMARY KEY,
+    name1 VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id NUMBER PRIMARY KEY,
+    user_id NUMBER,
+    payment_method_id NUMBER,
+    address_id NUMBER,
+    order_date DATE NOT NULL,
+    total_price NUMBER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users1(user_id),
+    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(payment_method_id),
+    FOREIGN KEY (address_id) REFERENCES addresses(address_id)
+);
+
+CREATE TABLE products (
+    product_id NUMBER PRIMARY KEY,
+    title VARCHAR2(100) NOT NULL,
+    category_id NUMBER,
+    manufacturer_id NUMBER,
+    price NUMBER NOT NULL,
+    stock NUMBER NOT NULL,
+    creation_date DATE NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(manufacturer_id)
+);
+
+CREATE TABLE order_items (
+    order_item_id NUMBER PRIMARY KEY,
+    order_id NUMBER,
+    product_id NUMBER,
+    quantity NUMBER NOT NULL,
+    price NUMBER NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+
+CREATE TABLE product_details (
+    product_detail_id NUMBER PRIMARY KEY,
+    product_id NUMBER,
+    key1 VARCHAR2(50) NOT NULL,
+    value1 VARCHAR2(200) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 CREATE TABLE reviews (
-  review_id INT PRIMARY KEY,
-  customer_id INT,
-  product_id INT,
-  rating INT,
-  review_text TEXT,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-  FOREIGN KEY (product_id) REFERENCES products(product_id)
-);
-
-CREATE TABLE suppliers (
-  supplier_id INT PRIMARY KEY,
-  supplier_name VARCHAR(50),
-  contact_name VARCHAR(50),
-  address VARCHAR(200),
-  phone VARCHAR(20),
-  email VARCHAR(100)
+    review_id NUMBER PRIMARY KEY,
+    product_id NUMBER,
+    user_id NUMBER,
+    rating NUMBER NOT NULL,
+    comment1 VARCHAR2(400),
+    review_date DATE NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (user_id) REFERENCES users1(user_id)
 );
